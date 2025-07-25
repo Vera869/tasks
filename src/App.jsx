e26ede24198bebe0e2b9f02b019e41581c4c1d38
Vera869 <verea8910@gmail.com>
2025-07-25 13:55:17 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import './App.scss'
+
+function App() {
+ 
+  return (
+    <>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum modi rem nulla odit id ut distinctio molestias minus? Fugiat, eos?</div>
+       
+    </>
+  )
+}
+
+export default App
