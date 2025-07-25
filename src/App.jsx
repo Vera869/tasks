e26e40d06df76c6a5743c1440a4e74123551a10f
Vera869 <verea8910@gmail.com>
2025-07-25 15:17:00 +0700

    added todo layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 // import { useState } from 'react'
 
-import "./App.scss";
+import S from "./App.module.scss";
+import { Form } from "./components/form/Form";
+import { Todo } from "./components/todo/Todo";
 
 function App() {
   return (
-    <>
-      <div>
+    <main className={S.main}>
+      <section className={S.sectionForm}>
+        <h1 className={S.sectionFormHeader}>Create todos</h1>
         <Form />
-      </div>
-    </>
+      </section>
+      <section className={S.sectionTodos}>
+        <h2 className={S.sectionTodosHeader}>My todo list</h2>
+        <div className={S.sectionTodosItems}>
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+           <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+        </div>
+      </section>
+    </main>
   );
 }
 
---
+++ src/components/todo/Todo.jsx
@@ -0,0 +1,23 @@
+import S from "./todo.module.scss";
+
+export const Todo = () => {
+  return (
+    <div className={S.todosItem}>
+      <input className={S.todosItemCheckbox} type="checkbox" />
+      <div className={S.todosItemBox}>
+        <p className={S.todosItemTitle}>
+          Title Todo Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Iure laboriosam odio animi ipsam autem et maiores delectus consequatur
+          praesentium amet.
+        </p>
+        <p className={S.todosItemDescription}>
+          Description Todo Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Inventore distinctio qui veritatis recusandae adipisci quas ea
+          temporibus delectus placeat quos totam deleniti ab praesentium nihil
+          impedit optio repellat illum, dolores consectetur necessitatibus id
+          magnam. Sit eius amet possimus ea nesciunt.
+        </p>
+      </div>
+    </div>
+  );
+};
